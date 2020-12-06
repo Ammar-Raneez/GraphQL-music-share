e88c7ea260d6d495b1eb2d35c130a36f7a3ea824
Ammar <ammarraneez@gmail.com>
2020-12-06 18:05:11 +0530

    queuesonglist done

--- src/QueueSongList.js
+++ src/QueueSongList.js
@@ -1,9 +1,67 @@
+import { Avatar, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
 import React from 'react'
 
 function QueueSongList() {
+    const song = {
+        title: 'LUNE',
+        artist: 'MOON',
+        thumbnail: 'http://img.youtube.com/vi/--ZtUFsIgMk/0.jpg'
+    }
+
     return (
-        <div>
-            Queue List
+        <div style={{ margin: '10px 0' }}>
+            <Typography color="textSecondary" variant="button">
+                QUEUE (5)
+            </Typography>
+            {Array.from({ length: 5 }, () => song).map((song, i) => (
+                <QueuedSong key={i} song={song} />
+            ))}
+        </div>
+    )
+}
+
+const useStyles = makeStyles({
+    avatar: {
+        width: 44,
+        height: 44
+    },
+    text: {
+        textOverflow: 'ellipsis',
+        overflow: 'hidden'
+    },
+    container: {
+        display: 'grid',
+        gridAutoFlow: 'column',
+        gridTemplateColumns: '50px auto 50px',
+        gridGap: 12,
+        alignItems: 'center',
+        marginTop: 10
+    },
+    songInfoContainer: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap'
+    }
+})
+
+function QueuedSong({ song }) {
+    const classes = useStyles();
+    const { thumbnail, artist, title } = song
+
+    return(
+        <div className={classes.container}>
+            <Avatar className={classes.avatar} src={thumbnail} alt="Song Thumbnail" />
+            <div className={classes.songInfoContainer}>
+                <Typography className={classes.text} variant="subtitle2">
+                    {title}
+                </Typography>
+                <Typography className={classes.text} variant="body2" color="textSecondary">
+                    {artist}
+                </Typography>
+            </div>
+            <IconButton>
+                <Delete color="error" />
+            </IconButton>
         </div>
     )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
-import { purple, deepOrange } from '@material-ui/core/colors'
+import { deepOrange, deepPurple } from '@material-ui/core/colors'
 import { createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
     palette: {
         type: "dark",
         primary: deepOrange,
-        secondary: purple
+        secondary: deepPurple
     }
 })
 
